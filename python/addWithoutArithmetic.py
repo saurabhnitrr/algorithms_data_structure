9e577f3f24bb1c62489f77edce468b8d0fdae849
saurabh <saurabhpathak997@gmail.com>
2020-04-28 20:35:52 +0530

    restructuring

--- python/addWithoutArithmetic.py
+++
@@ -1,9 +0,0 @@
-def add(x,y):
-    while(y!=0):
-         carry = x&y
-         x= x^y
-         y= carry<<1
-    return x
-    							
-print(add(10,22))
-#result = 32
--- python/choclateDistribution.py
+++
@@ -1,9 +0,0 @@
-for t in range(int(input())):
-    n = int(input())
-    a = list(map(int,input().split()))
-    k = int(input())
-    a.sort()
-    dif = a[k-1]-a[0]
-    for i in range(1,n-k+1):
-        dif = min(a[i+k-1]-a[i],dif)
-    print(dif)    
